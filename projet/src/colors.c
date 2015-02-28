72644e2f6ee5b28a432bb46cfccbc4d3089d8b54
vchaillo <valentinchaillou89@gmail.com>
2015-02-28 21:07:42 +0100

    maj menu et colors

--- projet/src/colors.c
+++ projet/src/colors.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/28 17:59:56 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/02/28 18:03:43 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 21:06:07 by vchaillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 void	init_colors(void)
 {
 	start_color();
+	init_pair(1, COLOR_GREEN, COLOR_GREEN);
+	init_pair(2, COLOR_GREEN, COLOR_BLACK);
+	init_pair(1, COLOR_GREEN, COLOR_GREEN);
+	init_pair(1, COLOR_GREEN, COLOR_GREEN);
 	init_pair(1, COLOR_GREEN, COLOR_GREEN);
 }
