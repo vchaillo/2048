acdbd8cffcff57205b6ac630e804ee9b2db53ae3
Asylum64 <jeanlaurent.barbie@gmail.com>
2015-02-28 15:47:16 +0100

    Adding struct

--- projet/includes/game.h
+++ projet/includes/game.h
@@ -6,15 +6,14 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:15:53 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/02/28 00:18:36 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 15:36:08 by jbarbie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GAME_H
 # define GAME_H
-
-#include <curses.h>
-# include "../libft/includes/libft.h"
+# include <curses.h>
+# include "libft.h"
 
 # define ESCAPE 27
 
@@ -22,6 +21,12 @@ enum	e_const
 {
 	WIN_VALUE = 2048
 };
+
+typedef struct	s_env
+{
+	int		nb_lines;
+	int		nb_columns;
+}				t_env;
 
 void	draw_grid(void);
 
