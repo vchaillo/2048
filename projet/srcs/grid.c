c3e7dcd8a1a496fcdc78558a3032d9224d12040a
Asylum64 <jeanlaurent.barbie@gmail.com>
2015-02-28 17:49:37 +0100

    Coucou Valou

--- projet/srcs/grid.c
+++ projet/srcs/grid.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:19:30 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/02/28 16:48:29 by jbarbie          ###   ########.fr       */
+/*   Updated: 2015/02/28 17:46:52 by jbarbie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,14 @@
 
 static void	get_size_case(t_env *e)
 {
-	e->case_h = e->nb_lines - (e->nb_lines % 4) / 4; 
-	e->case_w = e->nb_columns - (e->nb_columns % 4) / 4; 
+	e->case_h = ((e->nb_lines - 1) - (e->nb_lines % 4)) / 4; 
+	e->case_w = ((e->nb_columns - 1) - (e->nb_columns % 4)) / 4; 
+	if (e->case_w < 10 || e->case_h < 5)
+	{
+		endwin();
+		write(2, "Window too small\n", 17);
+		exit (0);
+	}
 }
 
 void	draw_grid(t_env *e)
@@ -26,19 +32,20 @@ void	draw_grid(t_env *e)
 	get_size_case(e);
 	attron(COLOR_PAIR(1));
 	i = 0;
-	while (i <= 5)
+	move(0, 0);
+	while (i < 5)
 	{
-		whline(stdscr, '-', e->nb_columns - (e->nb_columns % 4));
-		move(e->nb_lines / 4 * i, 0);
+		whline(stdscr, '-', e->case_w * 4);
 		i++;
+		move(e->case_h * i, 0);
 	}
 	i = 0;
-	while (i <= 5)
+	move(0, 0);
+	while (i < 5)
 	{
-		wvline(stdscr, '|', e->nb_lines - (e->nb_lines % 5) + 1);
-		move(0, (e->nb_columns / 4) * i);
+		wvline(stdscr, '|', e->case_h * 4 + 1);
 		i++;
+		move(0, e->case_w * i);
 	}
-	e->nb_lines++;
 	attroff(COLOR_PAIR(1));
 }
--- projet/srcs/main.c
+++ projet/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:02:56 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/02/28 16:57:32 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/02/28 17:43:49 by jbarbie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@ int		main(void)
 	noecho();
 	start_color();
 	init_pair(1, COLOR_GREEN, COLOR_GREEN);
-	draw_menu(&e);
 	getmaxyx(stdscr, e.nb_lines, e.nb_columns);
+	draw_menu(&e);
 	while ((key = getch()) != ESCAPE)
 	{
 		clear();
