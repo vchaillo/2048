61c0422ce0f8c57629e225cd438d2186a192fa7d
Asylum64 <jeanlaurent.barbie@gmail.com>
2015-03-01 02:14:51 +0100

    Coucou jte move.c

--- projet/src/move.c
+++ projet/src/move.c
@@ -6,7 +6,7 @@
 /*   By: jbarbie <jbarbie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/01 00:23:48 by jbarbie           #+#    #+#             */
-/*   Updated: 2015/03/01 02:08:58 by jbarbie          ###   ########.fr       */
+/*   Updated: 2015/03/01 02:14:26 by jbarbie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,7 +37,7 @@ void	move_up(t_env *e)
 		}
 		y = 1;
 		x++;
-	}		
+	}
 }
 
 void	move_down(t_env *e)
@@ -70,29 +70,29 @@ void	move_down(t_env *e)
 
 void	move_left(t_env *e)
 {
-		int	y;
+	int	y;
 	int	x;
 
-	y = 1;
-	x = 0;
-	while (x < 4)
+	y = 0;
+	x = 1;
+	while (y < 4)
 	{
-		while (y < 4)
+		while (x < 4)
 		{
-			if (e->grid[x][y - 1] == 0)
+			if (e->grid[x - 1][y] == 0)
 			{
-				e->grid[x][y - 1] = e->grid[x][y];
+				e->grid[x - 1][y] = e->grid[x][y];
 				e->grid[x][y] = 0;
 			}
-			else if (e->grid[x][y - 1] == e->grid[x][y])
+			else if (e->grid[x - 1][y] == e->grid[x][y])
 			{
-				e->grid[x][y - 1] *= 2;
+				e->grid[x - 1][y] *= 2;
 				e->grid[x][y] = 0;
 			}
-			y++;
+			x++;
 		}
-		y = 1;
-		x++;
+		x = 1;
+		y++;
 	}
 }
 
@@ -101,25 +101,25 @@ void	move_right(t_env *e)
 	int	y;
 	int	x;
 
-	y = 1;
-	x = 0;
-	while (x < 4)
+	y = 0;
+	x = 2;
+	while (y < 4)
 	{
-		while (y < 4)
+		while (x >= 0)
 		{
-			if (e->grid[x][y - 1] == 0)
+			if (e->grid[x + 1][y] == 0)
 			{
-				e->grid[x][y - 1] = e->grid[x][y];
+				e->grid[x + 1][y] = e->grid[x][y];
 				e->grid[x][y] = 0;
 			}
-			else if (e->grid[x][y - 1] == e->grid[x][y])
+			else if (e->grid[x + 1][y] == e->grid[x][y])
 			{
-				e->grid[x][y - 1] *= 2;
+				e->grid[x + 1][y] *= 2;
 				e->grid[x][y] = 0;
 			}
-			y++;
+			x--;
 		}
-		y = 1;
-		x++;
+		x = 2;
+		y++;
 	}
 }
