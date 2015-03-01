917c72c75f1b210ce43fed8eb431c36f7b23385e
vchaillo <valentinchaillou89@gmail.com>
2015-03-01 22:27:56 +0100

    vraiment plus que win loose

--- projet/src/grid.c
+++ projet/src/grid.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:19:30 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/03/01 21:34:53 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/03/01 22:21:27 by vchaillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,24 +18,11 @@ static void	print_help(t_env *e)
 	printw("Up,Down,Right,Left,Echap.");
 }
 
-static void	get_size_case(t_env *e)
-{
-	e->ch = ((e->nb_lines - 2) - (e->nb_lines % 4)) / 4;
-	e->cw = ((e->nb_columns - 1) - (e->nb_columns % 4)) / 4;
-	if (e->cw < 10 || e->ch < 5)
-	{
-		endwin();
-		write(2, "Window too small\n", 17);
-		exit (0);
-	}
-}
-
 void		draw_grid(t_env *e)
 {
 	int		i;
 
 	getmaxyx(stdscr, e->nb_lines, e->nb_columns);
-	get_size_case(e);
 	print_help(e);
 	attron(COLOR_PAIR(1));
 	i = 0;
--- projet/src/main.c
+++ projet/src/main.c
@@ -6,7 +6,7 @@
 /*   By: vchaillo <vchaillo@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 22:02:56 by vchaillo          #+#    #+#             */
-/*   Updated: 2015/03/01 22:16:06 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/03/01 22:26:46 by vchaillo         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,20 @@ static int		check_win_value(void)
 	return (TRUE);
 }
 
-static void		do_game(t_env *e, int key)
+static int		get_size_case(t_env *e)
+{
+	e->ch = ((e->nb_lines - 2) - (e->nb_lines % 4)) / 4;
+	e->cw = ((e->nb_columns - 1) - (e->nb_columns % 4)) / 4;
+	if (e->cw < 10 || e->ch < 5)
+		return (FALSE);
+	return (TRUE);
+}
+
+static int		do_game(t_env *e, int key)
 {
 	clear();
+	if (!(get_size_case(e)))
+		return (FALSE);
 	draw_grid(e);
 	e->move = FALSE;
 	if (e->init == FALSE)
@@ -41,12 +52,14 @@ static void		do_game(t_env *e, int key)
 	}
 	color_cases(e);
 	e->init = FALSE;
+	return (TRUE);
 }
 
 int				main(void)
 {
 	int			key;
 	t_env		e;
+	int			val;
 
 	initscr();
 	raw();
@@ -58,8 +71,14 @@ int				main(void)
 	getmaxyx(stdscr, e.nb_lines, e.nb_columns);
 	draw_menu(&e);
 	while ((key = getch()) != ESCAPE && check_win_value())
-		do_game(&e, key);
+	{
+		val = do_game(&e, key);
+		if (val == FALSE)
+			break ;
+	}
 	endwin();
+	if (val == FALSE)
+		write(2, "Window is too small\n", 20);
 	if (!(check_win_value()))
 		write(2, "Incorrect WIN_VALUE\n", 20);
 	return (0);
