ae0094a0243664fc8eb8c6d7f05b758537b03500
Asylum64 <jeanlaurent.barbie@gmail.com>
2015-03-01 02:09:26 +0100

    Adding Move

--- projet/src/move.c
+++ projet/src/move.c
@@ -6,7 +6,7 @@
 /*   By: jbarbie <jbarbie@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/03/01 00:23:48 by jbarbie           #+#    #+#             */
-/*   Updated: 2015/03/01 00:39:03 by vchaillo         ###   ########.fr       */
+/*   Updated: 2015/03/01 02:08:58 by jbarbie          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,20 +14,112 @@
 
 void	move_up(t_env *e)
 {
-	(void)e;
+	int	y;
+	int	x;
+
+	y = 1;
+	x = 0;
+	while (x < 4)
+	{
+		while (y < 4)
+		{
+			if (e->grid[x][y - 1] == 0)
+			{
+				e->grid[x][y - 1] = e->grid[x][y];
+				e->grid[x][y] = 0;
+			}
+			else if (e->grid[x][y - 1] == e->grid[x][y])
+			{
+				e->grid[x][y - 1] *= 2;
+				e->grid[x][y] = 0;
+			}
+			y++;
+		}
+		y = 1;
+		x++;
+	}		
 }
 
 void	move_down(t_env *e)
 {
-	(void)e;
+	int	y;
+	int	x;
+
+	y = 2;
+	x = 0;
+	while (x < 4)
+	{
+		while (y >= 0)
+		{
+			if (e->grid[x][y + 1] == 0)
+			{
+				e->grid[x][y + 1] = e->grid[x][y];
+				e->grid[x][y] = 0;
+			}
+			else if (e->grid[x][y + 1] == e->grid[x][y])
+			{
+				e->grid[x][y + 1] *= 2;
+				e->grid[x][y] = 0;
+			}
+			y--;
+		}
+		y = 2;
+		x++;
+	}
 }
 
 void	move_left(t_env *e)
 {
-	(void)e;
+		int	y;
+	int	x;
+
+	y = 1;
+	x = 0;
+	while (x < 4)
+	{
+		while (y < 4)
+		{
+			if (e->grid[x][y - 1] == 0)
+			{
+				e->grid[x][y - 1] = e->grid[x][y];
+				e->grid[x][y] = 0;
+			}
+			else if (e->grid[x][y - 1] == e->grid[x][y])
+			{
+				e->grid[x][y - 1] *= 2;
+				e->grid[x][y] = 0;
+			}
+			y++;
+		}
+		y = 1;
+		x++;
+	}
 }
 
 void	move_right(t_env *e)
 {
-	(void)e;
+	int	y;
+	int	x;
+
+	y = 1;
+	x = 0;
+	while (x < 4)
+	{
+		while (y < 4)
+		{
+			if (e->grid[x][y - 1] == 0)
+			{
+				e->grid[x][y - 1] = e->grid[x][y];
+				e->grid[x][y] = 0;
+			}
+			else if (e->grid[x][y - 1] == e->grid[x][y])
+			{
+				e->grid[x][y - 1] *= 2;
+				e->grid[x][y] = 0;
+			}
+			y++;
+		}
+		y = 1;
+		x++;
+	}
 }
